a8573c7dae94ea421cc16b2eeb6e567616599a7d
balkiss <balkiss.ghanmi@esprit.tn>
2024-03-29 17:11:22 +0100

    test

--- sum.go
+++ sum.go
@@ -1,12 +1,7 @@
-package sum
+package main
 
-// Ints returns the sum of a list of integers. func Ints(vs ...int) int {
-func Ints(vs ...int) int {
-	return ints(vs)
-}
-func ints(vs []int) int {
-	if len(vs) == 0 {
-		return 0
-	}
-	return ints(vs[1:]) + vs[0]
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
 }
