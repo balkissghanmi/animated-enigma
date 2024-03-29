f858354af764f46f38b986ca224991b09d8f29a9
balkiss <balkiss.ghanmi@esprit.tn>
2024-03-29 18:06:28 +0100

    test

--- sum.go
+++ sum.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-// make subTotalBill as global variable to make it easily accessible in case customer modifies her order.
-// subTotalBill means total bill but excluding taxes.
-var subTotalBill float64
-
-// make a map of customerOrder in which we will store the items ordered as "key" and no. of plates as "value".
-var customerOrder = make(map[string]uint, 0)
+import "fmt"
 
 func main() {
-	var customerName string
-	fmt.Println("What is your first name?")
-	fmt.Scan(&customerName)
-
+	fmt.Println("Hello, World!")
 }
