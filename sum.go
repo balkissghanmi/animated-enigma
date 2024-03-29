7d55389a08a3723b4b86c665bcac6e1491637c80
balkiss <balkiss.ghanmi@esprit.tn>
2024-03-29 18:01:07 +0100

    test

--- sum.go
+++ sum.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
+// make subTotalBill as global variable to make it easily accessible in case customer modifies her order.
+// subTotalBill means total bill but excluding taxes.
+var subTotalBill float64
+
+// make a map of customerOrder in which we will store the items ordered as "key" and no. of plates as "value".
+var customerOrder = make(map[string]uint, 0)
+
 func main() {
-	fmt.Println(quote.Go())
+	var customerName string
+	fmt.Println("What is your first name?")
+	fmt.Scan(&customerName)
+
 }
